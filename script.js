0d75b1c8d8113579f09ba610ac674f20766c5925
Ricardo-Tanabe <uhtanabe@gmail.com>
2025-01-02 14:56:14 -0300

    Reordenação dos elementos a cada interação concluído. Os elementos também são salvos em localStorage.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let taskList = [
-    "New task is created and added to the list",
-    "Clicking the checkbox toggles the completeness",
-    "Delete button will delete the task from the list",
-    "Complete tasks show at the end with strikethrough",
-    "Marking in complete will put it back in pending list",
-    "Exemplo teste"
+    {text: "New task is created and added to the list", isChecked: false},
+    {text: "Clicking the checkbox toggles the completeness", isChecked: false},
+    {text: "Delete button will delete the task from the list", isChecked: false},
+    {text: "Complete tasks show at the end with strikethrough", isChecked: false},
+    {text: "Marking in complete will put it back in pending list", isChecked: false},
+    {text: "Exemplo teste", isChecked: false}
 ]
 
 const taskContainer = document.querySelector(".task-container");
@@ -12,9 +12,7 @@ const inputBar = document.querySelector('.input-container');
 const inputAddTask = inputBar.children[0];
 const enterButton = inputBar.children[1];
 
-function createTask(innerText) {
-    const newTaks = document.createElement('li');
-
+function createInnerElementsInChildren(innerText) {
     const checkbox = document.createElement('input');
     const task = document.createElement('span');
     const deleteIcon = document.createElement('img');
@@ -27,17 +25,60 @@ function createTask(innerText) {
     deleteIcon.src = './trash-can.png';
     deleteIcon.alt = 'trash icon';
 
-    newTaks.appendChild(checkbox);
-    newTaks.appendChild(task);
-    newTaks.appendChild(deleteIcon);
-
-    taskContainer.appendChild(newTaks);
+    return [checkbox, task, deleteIcon]
 }
 
-function fillChildrenLi() {
-    for(let i = 0; i < taskList.length; i++) {
-        createTask(taskList[i]);
+function createChildren(innerElements) {
+    const newTask = document.createElement('li');
+    for (let i = 0; i < innerElements.length; i++) {
+        newTask.appendChild(innerElements[i]);
     }
+    return newTask
+}
+
+function createTask(innerText) {
+    const innerElements = createInnerElementsInChildren(innerText)
+    const newTask = createChildren(innerElements);
+    taskContainer.appendChild(newTask);
+}
+
+function deleteOldTasks() {
+    while (taskContainer.firstChild) {
+        taskContainer.removeChild(taskContainer.firstChild);
+    }
+}
+
+function orderedNonCheckedChildren() {
+    let checkedArray = []
+    for(let i = 0; i < taskList.length; i++) {
+        if(!taskList[i].isChecked) {
+            createTask(taskList[i].text);
+            continue
+        }
+        checkedArray.push(taskList[i])
+    }
+    return checkedArray
+}
+
+function orderedCheckedChildren(checkedArray, difference) {
+    let reorderedList = difference
+    for(let i = 0; i < checkedArray.length; i++) {
+        createTask(checkedArray[i].text);
+        const checkbox = taskContainer.lastChild.children[0];
+        const task = taskContainer.lastChild.children[1];
+        task.style.textDecoration = 'line-through';
+        task.style.color = 'rgb(150, 150, 150)';
+        checkbox.checked = checkedArray[i].isChecked;
+        reorderedList.push(checkedArray[i])
+    }
+    return reorderedList
+}
+
+function renderTasks() {
+    deleteOldTasks()
+    let checkedArray = orderedNonCheckedChildren()
+    let difference = taskList.filter(item => !checkedArray.includes(item));
+    taskList = orderedCheckedChildren(checkedArray, difference)
 }
 
 function appleAnimation() {
@@ -53,22 +94,50 @@ function appleAnimation() {
     }
 }
 
-function changeTextStyle(checkbox) {
-    const textFieldStyle = checkbox.nextSibling.style
-    if(checkbox.checked) {
-        textFieldStyle.textDecoration = 'line-through';
-        textFieldStyle.color = 'rgb(150, 150, 150)';
-        return true
-    }
-    textFieldStyle.textDecoration = 'none';
-    textFieldStyle.color = 'rgb(0, 0, 0)';
-    return false
-}
-
 function inputValidation() {
     if(inputAddTask.value) {
-        createTask(inputAddTask.value);
+        taskList.push({text: inputAddTask.value, isChecked: false})
+        deleteOldTasks()
+        renderTasks()
+        appleAnimation()
         inputAddTask.value = '';
+    }
+}
+
+function selectedChildren(checkbox) {
+    let childrens = Array.from(taskContainer.children);
+    let currentElement = checkbox.parentElement;
+    let index = childrens.indexOf(currentElement);
+    let children = childrens[index];
+    let span = children.children[1]
+    return [checkbox, children, span, index];
+}
+
+function changeElementsPosition(checkbox, children, span, index) {
+    taskList.splice(index, 1);
+    if(checkbox.checked) {
+        taskContainer.appendChild(children);
+        taskList.splice(taskList.length, 0, {text: span.innerHTML, isChecked: true});
+    } else {
+        if(taskContainer.firstChild) {
+            taskContainer.insertBefore(children, taskContainer.firstChild);
+        } else {
+            taskContainer.appendChild(children);
+        }
+        taskList.splice(0, 0, {text: span.innerHTML, isChecked: false});
+    }
+}
+
+function saveChange() {
+    let taskListString = JSON.stringify(taskList);
+    localStorage.setItem('taskListKey', taskListString)
+}
+
+function loadChange() {
+    console.log(localStorage.getItem('taskListKey'));
+    if(localStorage.getItem('taskListKey') !== null) {
+        let taskListString = localStorage.getItem('taskListKey')
+        taskList = JSON.parse(taskListString)
     }
 }
 
@@ -76,47 +145,37 @@ document.addEventListener('keydown', (e) => {
     if(e.key === 'Enter') {
         inputValidation()
         appleAnimation()
-        // Criar função que acrecenta um elemento em taskList
     }
 
+    saveChange()
 })
 
 document.addEventListener('click', (e)=> {
     if(e.target.classList.contains("checked")) {
-        const checkbox = e.target;
-        let styleDecoration = changeTextStyle(checkbox);
-        var children = Array.from(taskContainer.children);
-        var currentElement = checkbox.parentElement;
-        var index = children.indexOf(currentElement);
-        let tempChildren = taskContainer.children[index];
-        taskContainer.removeChild(taskContainer.children[index]);
-        if(styleDecoration) {
-            taskContainer.appendChild(tempChildren);
-        } else {
-            if(taskContainer.firstChild) {
-                taskContainer.insertBefore(tempChildren, taskContainer.firstChild);
-            } else {
-                taskContainer.appendChild(tempChildren);
-            }
-        }
+        let args = selectedChildren(e.target)
+        changeElementsPosition(...args)
+        renderTasks()
         appleAnimation()
-        // Criar função que reordena os elementos em taskList
     }
 
     if(e.target.classList.contains("delete")) {
-        const liToRemove = e.target.parentElement;
-        taskContainer.removeChild(liToRemove);
-        // Criar função que elimina o elemento determinado em taskList        
+        const childRemove = e.target.parentElement;
+        const index = Array.prototype.indexOf.call(taskContainer.children, childRemove)
+        taskList.splice(index, 1)
+        taskContainer.removeChild(childRemove);
     }
 
     if(e.target === enterButton) {
-        inputValidation()
-        appleAnimation()
-        // Criar função que acrecenta um elemento em taskList
+        inputValidation();
+        appleAnimation();
     }
+
+    saveChange()
 })
 
 document.addEventListener('DOMContentLoaded', () => {
-    fillChildrenLi()
+    // localStorage.clear(); // Usado para limpar o cache
+    loadChange()
+    renderTasks()
     appleAnimation()
 })
